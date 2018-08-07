5217aac14cd3701259e705035adc41b14cef340b
Dubois Grayson <Grayson.Dubois@hcahealthcare.com>
2018-08-07 17:39:35 -0500

    Added babel, webpack, and jest

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const path = require('path')
+const webpack = require('webpack')
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    target: 'node',
+}
